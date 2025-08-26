9486163aab3700f1d2ee5b7276fbf39d0a832937
Majid Hussain <122609422+Maju-java-developer@users.noreply.github.com>
2025-08-26 17:07:22 +0500

    Test Message for specific file commit

--- common/src/main/java/util/Constants.java
+++ common/src/main/java/util/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
     // JWT Token Claims
     public static final String USER_RESPONSE_CLAIM = "userRespDTO";
     public static final String USER_AGENT_CLAIM = "User-Agent";
+    public static final String USER_AGENT_CLAI = "User-Agent";
 
     // Can Access endpoint without token
     public static String[] WHITELIST_ENDPOINTS = new String[]{"/auth/login", "/auth/register", "/actuator/*"};
