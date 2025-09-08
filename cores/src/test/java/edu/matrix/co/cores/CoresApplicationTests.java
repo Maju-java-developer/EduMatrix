ea59bc351f7ce17085df87854f820d187652ae2b
Majid Hussain <122609422+Maju-java-developer@users.noreply.github.com>
2025-09-08 15:25:31 +0500

    create cores module and set module for parent version

---
+++ cores/src/test/java/edu/matrix/co/cores/CoresApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.matrix.co.cores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
